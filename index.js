28f9fd40ebc93631092a3c88e53688b655d24408
Adam Murray <adamjmurray@gmail.com>
2015-07-26 16:11:37 -0700

    introduced push-logger and cleaned up the input handling via the observer pattern

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 // run with node-babel for ES6 syntax!
 
-import {PushMidiIn} from './lib/push-midi-in';
-import {PushMidiOut} from './lib/push-midi-out';
-import {COLORS} from './lib/push-colors';
+import {PushInput} from './lib/push-input';
+import {PushOutput} from './lib/push-output';
+import {PushLogger} from './lib/push-logger';
+import {PushColors} from './lib/push-colors';
 import * as PushText from './lib/push-text';
 
 let portName = 'Ableton Push User Port',
-    pushMidiIn = new PushMidiIn(portName),
-    pushMidiOut = new PushMidiOut(portName);
+    pushInput = new PushInput(portName),
+    pushOutput = new PushOutput(portName),
+    log = console.log,
+    {highlight,dim,dimmer} = PushColors;
 
-//for(let y=0; y<8; y++) {
-//  for(let x=0; x<8; x++) {
-//    pushMidiOut.gridButton(x,y,x+y*8);
-//  }
-//}
+pushOutput.grid.clear();
+pushOutput.lcd.clear();
 
-pushMidiOut.gridClear();
+//pushOutput.grid.button(0,0, PushColors.RED);
+//pushOutput.grid.button(1,0, highlight(PushColors.RED));
+//pushOutput.grid.button(0,1, dim(PushColors.RED));
+//pushOutput.grid.button(0,2, dimmer(PushColors.RED));
+//
+//pushOutput.lcd.row(0, 'hello world');
 
-pushMidiOut.gridButton(0,0, COLORS.RED);
-pushMidiOut.gridButton(1,0, COLORS.highlight(COLORS.RED));
-pushMidiOut.gridButton(0,1, COLORS.dim(COLORS.RED));
-pushMidiOut.gridButton(0,2, COLORS.dimmer(COLORS.RED));
-
-pushMidiOut.lcdRow(0, 'hello world');
-pushMidiOut.lcdRow(1, 'just testing');
-pushMidiOut.lcdRow(2, Object.values(PushText.CHARS).join(''));
-pushMidiOut.lcdRow(3, '↑↓≡┣┫║╍⏍¦°ÄÇÖÜßàäçèéêîñö÷øü♭…█→←▶');
+new PushLogger(pushInput); // , {exclude:'grid.press'});
 
